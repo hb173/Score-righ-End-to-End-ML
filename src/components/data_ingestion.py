1d4e810d496e676e7325dfe8f0efe354642cad99
hb173 <hb173@duke.edu>
2023-04-17 17:49:24 -0400

    ingestion completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,44 @@
-'''
-componets is a module 
-can add many more files like validation but keeping it simple
-data ingestion is reading the data from data base or files,
-'''
+import os
+import sys
+path=os.path.abspath("/Users/himangshu/Documents/mlproject/src/")
+sys.path.append(path)
+from exception import CustomException
+from logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifacts','test.csv')
+    raw_data_path:str=os.path.join('artifacts','data.csv')
+
+class DataIngestion:
+    def __init__ (self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+    
+        logging.info("Entered the data ingestion method or componet")
+        try:
+            df=pd.read_csv('/Users/himangshu/documents/mlproject/notebook/data/stud.csv')
+            #print(df)
+            logging.info('Exported the dataset as dataframe')
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            logging.info("Train test split intiated")
+            train_set,test_set=train_test_split(df,test_size=0.2)#,index=False,header=True)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Ingestion of the data is completed")
+
+            return(self.ingestion_config.train_data_path,self.ingestion_config.test_data_path)
+        except Exception as e:
+            pass
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
