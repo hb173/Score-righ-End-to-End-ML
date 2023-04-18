646ea6fec6681bcc2312d2cda3b202aef5e86995
hb173 <hb173@duke.edu>
2023-04-17 21:46:27 -0400

    Model_trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -14,8 +14,8 @@ from data_transformation import DataTransformation
 from data_transformation import DataTransformationConfig
 
 
-#from src.components.model_trainer import ModelTrainerConfig
-#from src.components.model_trainer import ModelTrainer
+from model_trainer import ModelTrainerConfig
+from model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -59,3 +59,6 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
